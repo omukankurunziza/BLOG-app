e02484ca7dc50f8babdf2a444396282a38bb0ec5
mukankunziza opportunee <nshutioppo@yahoo.fr>
2019-03-03 16:51:59 +0200

    add app folder

---
+++ start.sh
@@ -0,0 +1,6 @@
+export MOVIE_API_KEY='7854ce99b646526f11604daf32dc1202'
+export SECRET_KEY='12345'
+
+export MAIL_USERNAME='nshutioppo1987@gmail.com'
+export MAIL_PASSWORD='rukundoppo'
+python3.6 manage.py server
---
+++ tests/test_user.py
@@ -0,0 +1,23 @@
+import unittest
+from app.models import User
+
+class UserModelTest(unittest.TestCase):
+
+    def setUp(self):
+        self.new_user = User(id = 1, password = 'oppo', username = 'wecode', email="nshutioppo1987@gmail.com", bio = 'i have to learn more', profile_pic_path='/pictures/oppo.JPG')
+
+    # def test_instance(self):
+    #     self.assertTrue(isinstance(self.new_user, User))
+
+    def test_password_setter(self):
+        self.assertTrue(self.new_user.password_hash is not None)
+
+    def test_no_access_password(self):
+        with self.assertRaises(AttributeError):
+            self.new_user.password
+
+    def test_password_verification(self):
+        self.assertTrue(self.new_user.verify_password('123'))
+
+if __name__ == '__main__':
+    unittest.main()
